288d03efba07b971f9b78cfe6d5de99291cb80a0
Dione Santos <dionedev@gmail.com>
2024-11-04 23:00:06 -0300

    feat: Product

--- src/main/java/com/springjava/course/entities/Product.java
+++ src/main/java/com/springjava/course/entities/Product.java
@@ -1,47 +1,45 @@
 package com.springjava.course.entities;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 @Entity
-@Table(name = "tb_user")
-public class User implements Serializable {
+@Table(name = "tb_product")
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	private String email;
-	private String phone;
-	private String password;
+	private String description;
+	private Double price;
+	private String imageUrl;
 	
-	@JsonIgnore
-	@OneToMany(mappedBy = "client")
-	private List<Order> orders = new ArrayList<>();
+	@Transient
+	private Set<Category> categories = new HashSet<>(); 
 	
-	public User() {
+	public Product() {
 		
 	}
 
-	public User(Long id, String name, String email, String phone, String password) {
+	public Product(Long id, String name, String description, Double price, String imageUrl) {
 		super();
 		this.id = id;
 		this.name = name;
-		this.email = email;
-		this.phone = phone;
-		this.password = password;
+		this.description = description;
+		this.price = price;
+		this.imageUrl = imageUrl;
 	}
 
 	public Long getId() {
@@ -60,34 +58,33 @@ public class User implements Serializable {
 		this.name = name;
 	}
 
-	public String getEmail() {
-		return email;
+	public String getDescription() {
+		return description;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
-	public String getPhone() {
-		return phone;
+	public Double getPrice() {
+		return price;
 	}
 
-	public void setPhone(String phone) {
-		this.phone = phone;
+	public void setPrice(Double price) {
+		this.price = price;
 	}
 
-	public String getPassword() {
-		return password;
+	public String getImageUrl() {
+		return imageUrl;
 	}
 
-	public void setPassword(String password) {
-		this.password = password;
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
 	}
-
-	public List<Order> getOrders() {
-		return orders;
+	
+	public Set<Category> getCategories() {
+		return categories;
 	}
-
 
 	@Override
 	public int hashCode() {
@@ -102,8 +99,7 @@ public class User implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		User other = (User) obj;
+		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
 	}
-
 }
--- src/main/java/com/springjava/course/resources/ProductResource.java
+++ src/main/java/com/springjava/course/resources/ProductResource.java
@@ -3,32 +3,32 @@ package com.springjava.course.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.ResponseEntity;
 
-import com.springjava.course.entities.Category;
-import com.springjava.course.services.CategoryService;
+import com.springjava.course.entities.Product;
+import com.springjava.course.services.ProductService;
 
 @RestController
-@RequestMapping(value = "/categories")
-public class CategoryResource {
+@RequestMapping(value = "/products")
+public class ProductResource {
 	
 	@Autowired
-	private CategoryService categoryService;
+	private ProductService productService;
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll() {
+	public ResponseEntity<List<Product>> findAll() {
 		
-		List<Category> list = categoryService.findAll();
+		List<Product> list = productService.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Category> findById(@PathVariable Long id){
-		Category obj = categoryService.findById(id);
+	public ResponseEntity<Product> findById(@PathVariable Long id){
+		Product obj = productService.findById(id);
 		
 		return ResponseEntity.ok().body(obj);
 	}
